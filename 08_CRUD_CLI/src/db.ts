da73dd339be9f78d4c5b6ce0e22e6df58425b4d5
onureredo <onureredo@yahoo.com>
2025-06-11 10:47:22 +0200

    crud_cli

---
+++ 08_CRUD_CLI/src/db.ts
@@ -0,0 +1,16 @@
+import { MongoClient } from 'mongodb';
+
+const MONGO_URI = process.env.MONGO_URI || '';
+const client = new MongoClient(MONGO_URI);
+
+(async () => {
+  try {
+    await client.connect();
+    console.log('✅ Connected to MongoDB');
+  } catch (err) {
+    console.error('❌ MongoDB connection error:', err);
+    process.exit(1);
+  }
+})();
+
+export const db = client.db('ecommerce');
